70044393c11c3c2b6efe8daa451528d5c9e39857
Peter Fonseca <peter.nfonseca@gmail.com>
2016-08-08 15:14:03 -0700

    no more alerting agent detection, maybe alter blocker on iphone?

--- bundle.js
+++ bundle.js
@@ -81,9 +81,14 @@
 	    safari = /safari/.test( userAgent ),
 	    ios = /iphone|ipod|ipad/.test( userAgent );
 	
-	if (ios) {
-	  alert("mobile safari detected");
-	  startGame();
+	if (!window.playing && ios) {
+	  clockEl.className = "clock";
+	  clockEl.innerHTML = "";
+	  endEl.className = "toplevel-wrapper hidden";
+	  startEl.className += " hidden";
+	
+	  window.playing = true;
+	  const gameView = new GameView(canvasEl.width, canvasEl.height, clockEl, endEl, endTimeEl).start(ctx);
 	}
 
 
